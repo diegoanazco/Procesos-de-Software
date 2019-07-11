462100c571a949104c5173e958f11556c2b3e484
diegoanazco <38145376+diegoanazco@users.noreply.github.com>
2019-07-10 21:25:22 -0500

    Add files via upload

---
+++ WebServiceSOAP/WebService_Oracle_Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oracle.DataAccess.Client;
+using System.Web.Services;
+using Oracle.DataAccess.Types;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace WcfService2
+{
+    public class Oracle_conection
+    {
+        OracleConnection oc;
+        string oradb = "DATA SOURCE=localhost:1521/xe;PERSIST SECURITY INFO=True;USER ID=UPROCESOS_USER; PASSWORD=admin"; // establece conexion con el servidor
+        public Oracle_conection()
+        {
+        }
+
+        public void EstablecerConnection ()
+        {
+            oc = new OracleConnection(oradb);
+            oc.Open();
+            
+        }
+
+        public OracleConnection GetConexion()
+        {
+            return oc;
+        }
+    }
+}
